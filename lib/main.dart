a41740bfee554ea2c26ef44f1839003ebfbbe490
Giovanni Walter <giovanni.walter@outlook.com>
2024-12-16 02:33:31 -0300

    feat: finished basic functionalities

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,4 @@
-import 'dart:convert';
-
-import 'package:dart_openai/dart_openai.dart';
-import 'package:dotenv/dotenv.dart';
+import 'package:ai_personal_trainer/service/get_response.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:provider/provider.dart';
@@ -19,7 +16,8 @@ class MyApp extends StatelessWidget {
         title: 'AI PersonalTrainer',
         theme: ThemeData(
           useMaterial3: true,
-          colorScheme: ColorScheme.fromSeed(seedColor: Colors.red),
+          colorScheme: ColorScheme.fromSeed(
+              seedColor: Colors.deepPurple, brightness: Brightness.light),
         ),
         home: MyHomePage(),
       ),
@@ -44,28 +42,64 @@ class WorkoutRequest {
 }
 
 class Exercise {
-  String name;
-  String description;
-  String reps;
+  late String name;
+  late String description;
+  late String reps;
+  bool isExpanded = false;
 
-  Exercise(this.name, this.description, this.reps);
+  Exercise({required this.name, required this.description, required this.reps});
+
+  factory Exercise.fromJson(Map<String, dynamic> json) {
+    return Exercise(
+        name: json['name'] as String,
+        description: json['description'] as String,
+        reps: json['reps'] as String);
+  }
 }
 
 class WorkoutDay {
-  String day;
-  String name;
-  String duration;
-  List<Exercise> exercises;
+  late String day;
+  late String duration;
+  late List<Exercise> exercises;
 
-  WorkoutDay(this.day, this.name, this.duration, this.exercises);
+  WorkoutDay(
+      {required this.day, required this.duration, required this.exercises});
+
+  factory WorkoutDay.fromJson(Map<String, dynamic> json) {
+    var exercisesJson = json['exercises'] as List;
+    List<Exercise> exercises =
+        exercisesJson.map((v) => Exercise.fromJson(v)).toList();
+    return WorkoutDay(
+        day: json['day'] as String,
+        duration: json['duration'] as String,
+        exercises: exercises);
+  }
 }
 
 class Workout {
-  String name;
-  String goals;
-  List<WorkoutDay> days;
+  late String name;
+  late List<String> goals;
+  late List<String> modalities;
+  late List<WorkoutDay> days;
 
-  Workout(this.name, this.goals, this.days);
+  Workout(
+      {required this.name,
+      required this.goals,
+      required this.modalities,
+      required this.days});
+
+  factory Workout.fromJson(
+      Map<String, dynamic> json, List<String> goals, List<String> modalities) {
+    var daysObjsJson = json['days'] as List;
+    List<WorkoutDay> days =
+        daysObjsJson.map((v) => WorkoutDay.fromJson(v)).toList();
+
+    return Workout(
+        name: json['name'] as String,
+        goals: goals,
+        modalities: modalities,
+        days: days);
+  }
 }
 
 class MyAppState extends ChangeNotifier {
@@ -76,6 +110,11 @@ class MyAppState extends ChangeNotifier {
     request = newReq;
     notifyListeners();
   }
+
+  void addWorkout(Workout workout) {
+    workouts.add(workout);
+    notifyListeners();
+  }
 }
 
 class MyHomePage extends StatefulWidget {
@@ -83,132 +122,281 @@ class MyHomePage extends StatefulWidget {
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
-Future<String> getResponse(WorkoutRequest request) async {
-  var env = DotEnv(includePlatformEnvironment: true)..load();
-  OpenAI.apiKey = env['API_KEY']!;
-  OpenAI.baseUrl = env['API_URL']!;
-
-  final systemMessage = OpenAIChatCompletionChoiceMessageModel(
-      role: OpenAIChatMessageRole.system,
-      content: [
-        OpenAIChatCompletionChoiceMessageContentItemModel.text(
-            env['SYS_PROMPT']!)
-      ]);
-
-  final userMessage = OpenAIChatCompletionChoiceMessageModel(
-      role: OpenAIChatMessageRole.user,
-      content: [
-        OpenAIChatCompletionChoiceMessageContentItemModel.text(
-            jsonEncode(request))
-      ]);
-
-  final requestMessages = [systemMessage, userMessage];
-
-  OpenAIChatCompletionModel response = await OpenAI.instance.chat.create(
-    model: "gpt-3.5-turbo",
-    responseFormat: {'type': 'json_object'},
-    seed: 6,
-    messages: requestMessages,
-    temperature: 0.2,
-  );
-
-  print(response.choices.first.message);
-  return response.choices.first.message.toString();
-}
-
 class _MyHomePageState extends State<MyHomePage> {
   int index = 0;
   @override
   Widget build(BuildContext context) {
+    var appState = context.watch<MyAppState>();
+    var workouts = appState.workouts;
     return Scaffold(
-      body: Column(
-        children: [
-          Text('AI Personal Trainer'),
-        ],
+      appBar: AppBar(
+        title: Text('AI Personal Trainer'),
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
       ),
+      body: workouts.isEmpty
+          ? Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [Text('You don\'t have any workouts yet')],
+              ),
+            )
+          : ListView(
+              children: workouts
+                  .map((w) => WorkoutCard(
+                        workout: w,
+                      ))
+                  .toList(),
+            ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () => showDialog(
-            context: context,
-            builder: (context) {
-              var canContinue = false;
-              var appState = context.watch<MyAppState>();
-              var days = appState.request.days;
-              var goals = appState.request.goals;
-              var modalities = appState.request.modalities;
-              var duration = appState.request.duration;
-
-              return StatefulBuilder(builder: (context, setState) {
-                Widget formStep;
-                switch (index) {
-                  case 0:
-                    formStep = WorkoutDaysForm();
-                    canContinue = days.isNotEmpty;
-                  case 1:
-                    formStep = WorkoutGoalsForm();
-                    canContinue = goals.isNotEmpty;
-                  case 2:
-                    formStep = WorkoutModalitiesForm();
-                    canContinue = modalities.isNotEmpty;
-                  case 3:
-                    formStep = WorkoutDurationForm();
-                    canContinue = duration != '0h00m';
-                  case 4:
-                    formStep = Center(child: CircularProgressIndicator());
-                  default:
-                    formStep = Placeholder();
-                }
-                return AlertDialog(
-                  title: Text('New Workout'),
-                  insetPadding: EdgeInsets.all(10),
-                  content: Container(
-                    width: MediaQuery.of(context).size.width - 100,
-                    padding: EdgeInsets.fromLTRB(10, 20, 10, 10),
-                    child: formStep,
-                  ),
-                  actions: [
-                    index > 0 && index < 4
-                        ? ElevatedButton(
-                            onPressed: () {
-                              setState(() {
-                                index--;
-                              });
-                            },
-                            child: Text('Back'))
-                        : SizedBox.shrink(),
-                    index < 4
-                        ? ElevatedButton(
-                            onPressed: index < 3 && canContinue
-                                ? () {
-                                    setState(() {
-                                      index++;
-                                    });
-                                  }
-                                : index == 3 && canContinue
-                                    ? () async {
-                                        setState(() {
-                                          index = 4;
-                                        });
-                                        await getResponse(appState.request);
-                                        if (context.mounted) {
-                                          setState(() {
-                                            index = 0;
-                                            appState.updateRequest(
-                                                WorkoutRequest(
-                                                    [], [], [], "0h00m"));
-                                          });
-                                          Navigator.pop(context, 'Success');
-                                        }
-                                      }
-                                    : null,
-                            child:
-                                Text(index < 3 ? 'Next' : 'Generate workout'))
-                        : SizedBox.shrink(),
-                  ],
-                );
-              });
-            }),
+        onPressed: () => createWorkoutForm(context),
         tooltip: 'New Workout',
         child: const Icon(Icons.add),
+      ),
+    );
+  }
+
+  Future<dynamic> createWorkoutForm(BuildContext context) {
+    return showDialog(
+        barrierDismissible: index == 4 ? false : true,
+        context: context,
+        builder: (context) {
+          var canContinue = false;
+          var appState = context.watch<MyAppState>();
+          var days = appState.request.days;
+          var goals = appState.request.goals;
+          var modalities = appState.request.modalities;
+          var duration = appState.request.duration;
+
+          return StatefulBuilder(builder: (context, setState) {
+            Widget formStep;
+            switch (index) {
+              case 0:
+                formStep = WorkoutDaysForm();
+                canContinue = days.isNotEmpty;
+              case 1:
+                formStep = WorkoutGoalsForm();
+                canContinue = goals.isNotEmpty;
+              case 2:
+                formStep = WorkoutModalitiesForm();
+                canContinue = modalities.isNotEmpty;
+              case 3:
+                formStep = WorkoutDurationForm();
+                canContinue = duration != '0h00m';
+              case 4:
+                formStep = Center(child: CircularProgressIndicator());
+              default:
+                formStep = Placeholder();
+            }
+            return AlertDialog(
+              title: index < 4 ? Text('New Workout') : SizedBox.shrink(),
+              insetPadding: EdgeInsets.all(10),
+              backgroundColor: index == 4
+                  ? Colors.transparent
+                  : Theme.of(context).dialogBackgroundColor,
+              content: SingleChildScrollView(
+                child: Container(
+                  width: MediaQuery.of(context).size.width - 100,
+                  padding: EdgeInsets.fromLTRB(10, 20, 10, 10),
+                  child: formStep,
+                ),
+              ),
+              actions: [
+                index > 0 && index < 4
+                    ? ElevatedButton(
+                        onPressed: () {
+                          setState(() {
+                            index--;
+                          });
+                        },
+                        child: Text('Back'))
+                    : SizedBox.shrink(),
+                index < 4
+                    ? ElevatedButton(
+                        onPressed: index < 3 && canContinue
+                            ? () {
+                                setState(() {
+                                  index++;
+                                });
+                              }
+                            : index == 3 && canContinue
+                                ? () async {
+                                    setState(() {
+                                      index = 4;
+                                    });
+                                    var newWorkout =
+                                        await getResponse(appState.request);
+                                    appState.addWorkout(newWorkout);
+                                    if (context.mounted) {
+                                      setState(() {
+                                        index = 0;
+                                        appState.updateRequest(WorkoutRequest(
+                                            [], [], [], "0h00m"));
+                                      });
+                                      Navigator.pop(context, 'Success');
+                                    }
+                                  }
+                                : null,
+                        child: Text(index < 3 ? 'Next' : 'Generate workout'))
+                    : SizedBox.shrink(),
+              ],
+            );
+          });
+        });
+  }
+}
+
+class WorkoutCard extends StatelessWidget {
+  const WorkoutCard({
+    super.key,
+    required this.workout,
+  });
+
+  final Workout workout;
+
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      child: Card(
+        child: ListTile(
+          title: Text(workout.name),
+          subtitle: Row(
+            children: [Text(workout.modalities.join(', '))],
+          ),
+          onTap: () {
+            Navigator.push(context, MaterialPageRoute(builder: (context) {
+              return WorkoutPage(workout: workout);
+            }));
+          },
+        ),
+      ),
+    );
+  }
+}
+
+class WorkoutPage extends StatelessWidget {
+  const WorkoutPage({
+    super.key,
+    required this.workout,
+  });
+
+  final Workout workout;
+
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+      initialIndex: 0,
+      length: workout.days.length,
+      child: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+          title: Text(workout.name),
+          bottom: TabBar(
+            tabs: workout.days
+                .map((d) => Tab(
+                      text: d.day,
+                    ))
+                .toList(),
+          ),
+        ),
+        body: TabBarView(
+          children: workout.days
+              .map((d) => WorkoutDayView(workout: workout, day: d))
+              .toList(),
+        ),
+      ),
+    );
+  }
+}
+
+class WorkoutDayView extends StatefulWidget {
+  const WorkoutDayView({super.key, required this.workout, required this.day});
+
+  final Workout workout;
+  final WorkoutDay day;
+
+  @override
+  State<WorkoutDayView> createState() => _WorkoutDayViewState();
+}
+
+class _WorkoutDayViewState extends State<WorkoutDayView> {
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.all(16.0),
+      child: Column(
+        children: [
+          Row(children: [
+            Text(
+              'Modalities: ',
+              style: TextStyle(fontWeight: FontWeight.bold),
+            ),
+            Text(
+              widget.workout.modalities.join(', '),
+              style: TextStyle(fontStyle: FontStyle.italic),
+            ),
+          ]),
+          Row(
+            children: [
+              Text(
+                'Goals: ',
+                style: TextStyle(fontWeight: FontWeight.bold),
+              ),
+              Text(
+                widget.workout.goals.join(', '),
+                style: TextStyle(fontStyle: FontStyle.italic),
+              )
+            ],
+          ),
+          Row(
+            children: [
+              Text(
+                'Duration: ',
+                style: TextStyle(fontWeight: FontWeight.bold),
+              ),
+              Text(
+                widget.day.duration,
+                style: TextStyle(fontStyle: FontStyle.italic),
+              )
+            ],
+          ),
+          SizedBox(
+            height: 10,
+          ),
+          ExpansionPanelList(
+            expansionCallback: (int index, bool isExpanded) {
+              setState(() {
+                widget.day.exercises[index].isExpanded = isExpanded;
+              });
+            },
+            children: widget.day.exercises
+                .map((e) => ExpansionPanel(
+                    canTapOnHeader: true,
+                    headerBuilder: (context, isExpanded) {
+                      return ListTile(
+                        title: Text(e.name),
+                      );
+                    },
+                    body: Padding(
+                      padding: EdgeInsets.all(16.0),
+                      child: Column(
+                        children: [
+                          Row(
+                            children: [
+                              Text(
+                                'Reps: ',
+                                style: TextStyle(fontWeight: FontWeight.bold),
+                              ),
+                              Text(e.reps)
+                            ],
+                          ),
+                          Text(e.description)
+                        ],
+                      ),
+                    ),
+                    isExpanded: e.isExpanded))
+                .toList(),
+          )
+        ],
       ),
     );
   }
---
+++ lib/service/get_response.dart
@@ -0,0 +1,42 @@
+import 'dart:convert';
+import 'package:ai_personal_trainer/main.dart';
+import 'package:dart_openai/dart_openai.dart';
+import 'package:dotenv/dotenv.dart';
+
+Future<Workout> getResponse(WorkoutRequest request) async {
+  var env = DotEnv(includePlatformEnvironment: true)..load();
+  OpenAI.apiKey = env['API_KEY']!;
+  OpenAI.baseUrl = env['API_URL']!;
+
+  final systemMessage = OpenAIChatCompletionChoiceMessageModel(
+      role: OpenAIChatMessageRole.system,
+      content: [
+        OpenAIChatCompletionChoiceMessageContentItemModel.text(
+            env['SYS_PROMPT']!)
+      ]);
+
+  final userMessage = OpenAIChatCompletionChoiceMessageModel(
+      role: OpenAIChatMessageRole.user,
+      content: [
+        OpenAIChatCompletionChoiceMessageContentItemModel.text(
+            jsonEncode(request))
+      ]);
+
+  final requestMessages = [systemMessage, userMessage];
+
+  OpenAIChatCompletionModel response = await OpenAI.instance.chat.create(
+    model: "gpt-3.5-turbo",
+    responseFormat: {'type': 'json_object'},
+    seed: 6,
+    messages: requestMessages,
+    temperature: 0.2,
+  );
+
+  print(response.choices.first.message.content![0].text);
+
+  final Map<String, dynamic> parsed =
+      jsonDecode(response.choices.first.message.content![0].text.toString());
+
+  var newWorkout = Workout.fromJson(parsed, request.goals, request.modalities);
+  return newWorkout;
+}
