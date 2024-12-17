7f82af593b97d1494cfb0a04c968c83506daa52a
Giovanni Walter <giovanni.walter@outlook.com>
2024-12-17 11:42:25 -0300

    fix: minor UI fixes

--- lib/main.dart
+++ lib/main.dart
@@ -259,8 +259,9 @@ class WorkoutCard extends StatelessWidget {
       child: Card(
         child: ListTile(
           title: Text(workout.name),
-          subtitle: Row(
-            children: [Text(workout.modalities.join(', '))],
+          subtitle: Text(
+            workout.modalities.join(', '),
+            overflow: TextOverflow.ellipsis,
           ),
           onTap: () {
             Navigator.push(context, MaterialPageRoute(builder: (context) {
@@ -321,82 +322,86 @@ class WorkoutDayView extends StatefulWidget {
 class _WorkoutDayViewState extends State<WorkoutDayView> {
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: EdgeInsets.all(16.0),
-      child: Column(
-        children: [
-          Row(children: [
-            Text(
-              'Modalities: ',
-              style: TextStyle(fontWeight: FontWeight.bold),
-            ),
-            Text(
-              widget.workout.modalities.join(', '),
-              style: TextStyle(fontStyle: FontStyle.italic),
-            ),
-          ]),
-          Row(
-            children: [
+    return SingleChildScrollView(
+      child: Padding(
+        padding: EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Wrap(children: [
               Text(
-                'Goals: ',
+                'Modalities: ',
                 style: TextStyle(fontWeight: FontWeight.bold),
               ),
               Text(
-                widget.workout.goals.join(', '),
+                widget.workout.modalities.join(', '),
                 style: TextStyle(fontStyle: FontStyle.italic),
-              )
-            ],
-          ),
-          Row(
-            children: [
-              Text(
-                'Duration: ',
-                style: TextStyle(fontWeight: FontWeight.bold),
               ),
-              Text(
-                widget.day.duration,
-                style: TextStyle(fontStyle: FontStyle.italic),
-              )
-            ],
-          ),
-          SizedBox(
-            height: 10,
-          ),
-          ExpansionPanelList(
-            expansionCallback: (int index, bool isExpanded) {
-              setState(() {
-                widget.day.exercises[index].isExpanded = isExpanded;
-              });
-            },
-            children: widget.day.exercises
-                .map((e) => ExpansionPanel(
-                    canTapOnHeader: true,
-                    headerBuilder: (context, isExpanded) {
-                      return ListTile(
-                        title: Text(e.name),
-                      );
-                    },
-                    body: Padding(
-                      padding: EdgeInsets.all(16.0),
-                      child: Column(
-                        children: [
-                          Row(
-                            children: [
-                              Text(
-                                'Reps: ',
-                                style: TextStyle(fontWeight: FontWeight.bold),
-                              ),
-                              Text(e.reps)
-                            ],
-                          ),
-                          Text(e.description)
-                        ],
+            ]),
+            Wrap(
+              children: [
+                Text(
+                  'Goals: ',
+                  style: TextStyle(fontWeight: FontWeight.bold),
+                ),
+                Text(
+                  widget.workout.goals.join(', '),
+                  style: TextStyle(fontStyle: FontStyle.italic),
+                )
+              ],
+            ),
+            Wrap(
+              children: [
+                Text(
+                  'Duration: ',
+                  style: TextStyle(fontWeight: FontWeight.bold),
+                ),
+                Text(
+                  widget.day.duration,
+                  style: TextStyle(fontStyle: FontStyle.italic),
+                )
+              ],
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            ExpansionPanelList(
+              expansionCallback: (int index, bool isExpanded) {
+                setState(() {
+                  widget.day.exercises[index].isExpanded = isExpanded;
+                });
+              },
+              children: widget.day.exercises
+                  .map((e) => ExpansionPanel(
+                      canTapOnHeader: true,
+                      headerBuilder: (context, isExpanded) {
+                        return ListTile(
+                          title: Text(e.name),
+                        );
+                      },
+                      body: Padding(
+                        padding: EdgeInsets.all(16.0),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Wrap(
+                              children: [
+                                Text(
+                                  'Reps: ',
+                                  style: TextStyle(fontWeight: FontWeight.bold),
+                                ),
+                                Text(e.reps)
+                              ],
+                            ),
+                            Text(e.description)
+                          ],
+                        ),
                       ),
-                    ),
-                    isExpanded: e.isExpanded))
-                .toList(),
-          )
-        ],
+                      isExpanded: e.isExpanded))
+                  .toList(),
+            )
+          ],
+        ),
       ),
     );
   }
@@ -613,8 +618,7 @@ class _WorkoutDurationFormState extends State<WorkoutDurationForm> {
     return Column(
       children: [
         Text('Select the maximum duration of the workout'),
-        Row(
-          mainAxisAlignment: MainAxisAlignment.center,
+        Wrap(
           children: [
             SizedBox(
               width: 100,
